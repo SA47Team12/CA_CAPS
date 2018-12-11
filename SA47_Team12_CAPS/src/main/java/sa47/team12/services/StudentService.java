7cda7a06ace20988d50171827f2c7bee42167ca4
parinita88 <42559695+parinita88@users.noreply.github.com>
2018-12-11 14:18:07 +0800

    Student Service update

--- SA47_Team12_CAPS/src/main/java/sa47/team12/services/StudentService.java
+++ SA47_Team12_CAPS/src/main/java/sa47/team12/services/StudentService.java
@@ -6,5 +6,9 @@ import sa47.team12.model.Student;
 
 public interface StudentService {
 
-ArrayList<Student> findAllStudent();
+	ArrayList<Student> findAllStudent();
+	Student findStudent(Integer id);
+	Student updateStudent(Student s);
+	Student createStudent(Student s);
+	void removeStudent(Student s);
 }
--- SA47_Team12_CAPS/src/main/java/sa47/team12/services/StudentServiceImpl.java
+++ SA47_Team12_CAPS/src/main/java/sa47/team12/services/StudentServiceImpl.java
@@ -14,16 +14,41 @@ import sa47.team12.repo.StudentRepository;
 @Service
 public class StudentServiceImpl implements StudentService {
 
-@Resource
-private StudentRepository stuRepository;
+	@Resource
+	private StudentRepository stuRepository;
 
-/* (non-Javadoc)
-* @see edu.iss.cats.service.EmployeeService#findEmployeesByManager(java.lang.String)
-*/
-@Override
-@Transactional
-public ArrayList<Student> findAllStudent() {
-return (ArrayList<Student>) stuRepository.findAll();
-}
+	/* (non-Javadoc)
+	 * @see edu.iss.cats.service.EmployeeService#findEmployeesByManager(java.lang.String)
+	 */
+	@Override
+	@Transactional
+	public ArrayList<Student> findAllStudent() {
+		return (ArrayList<Student>) stuRepository.findAll();
+	}
+	
+	@Override
+	@Transactional
+	public Student findStudent(Integer id) {
+		return (Student) stuRepository.findById(id).get();
+	}
+	@Override
+	@Transactional
+	public Student updateStudent(Student s)
+	{
+		return stuRepository.saveAndFlush(s);
+	}
+	
+	@Override
+	@Transactional
+	public Student createStudent(Student s)
+	{
+		return stuRepository.saveAndFlush(s);
+	}
+	@Override
+	@Transactional
+	public void removeStudent(Student s)
+	{
+		stuRepository.delete(s);
+	}
 
 }
