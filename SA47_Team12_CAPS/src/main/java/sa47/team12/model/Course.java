7d91c76f6ea4461983b43612ca353018f7a23c8d
wynta <wynta@172.17.34.176>
2018-12-11 14:01:29 +0800

    merging branches

--- SA47_Team12_CAPS/src/main/java/sa47/team12/model/Course.java
+++ SA47_Team12_CAPS/src/main/java/sa47/team12/model/Course.java
@@ -15,6 +15,8 @@ public class Course implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	//GenerationType.IDENTITY means this field will be auto-incremented and will get assigned an id automatically whenever we save a new order.
 	@Column(name="course_id")
 	private int courseId;
 
