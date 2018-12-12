26c6a1d8b0905fe95e60b8fa2ac8e106bd002640
parinita88 <42559695+parinita88@users.noreply.github.com>
2018-12-12 14:37:20 +0800

    Lecturer Changes

---
+++ SA47_Team12_CAPS/src/main/java/sa47/team12/repo/LecturerRepository.java
@@ -0,0 +1,10 @@
+package sa47.team12.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sa47.team12.model.Lecturer;
+
+public interface LecturerRepository extends JpaRepository<Lecturer, Integer> {
+
+
+}
