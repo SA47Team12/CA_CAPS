1d44574a0b0f5f5a45dde8ca3c46c05f98183b46
parinita88 <42559695+parinita88@users.noreply.github.com>
2018-12-13 17:39:37 +0800

    Lecturer view and validator

---
+++ SA47_Team12_CAPS_1312/src/main/java/sa47/team12/validator/LecturerValidator.java
@@ -0,0 +1,36 @@
+package sa47.team12.validator;
+
+//import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+
+import sa47.team12.model.Lecturer;
+
+
+@Component
+public class LecturerValidator implements org.springframework.validation.Validator {
+
+	public LecturerValidator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return Lecturer.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+		Lecturer s = (Lecturer) target;
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstname", "The first name cannot be empty.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastname", "The last name cannot be empty.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address", "The address cannot be empty.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "Email Id cannot be empty.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "phone", "Phone Number cannot be empty.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "Password cannot be empty.");
+}
+}
